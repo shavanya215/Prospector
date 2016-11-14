2804fcf2ecc571bc8904fa6625d79a912670c02f
shavanya215 <shavanya.215@gmail.com>
2016-11-14 11:28:48 -0500

    trying to fix

---
+++ Assets/__Scripts/FloatingScore.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingScore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
