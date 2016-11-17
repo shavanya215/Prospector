5b53910b840350e4b3d4b41dd07751ef6f99dd2c
shavanya215 <shavanya.215@gmail.com>
2016-11-17 13:00:50 -0500

    prospector

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
@@ -18,13 +19,17 @@ public class Prospector : MonoBehaviour
 
     static public Prospector S;
     static public int SCORE_FROM_PREV_ROUND = 0;
-
     static public int HIGH_SCORE = 0;
 
-    public Vector3 fsPosMid  = new Vector3(0.5f, 0.9f,  0);
-    public Vector3 fsPosRun  = new Vector3(0.5f, 0.75f, 0);
-    public Vector3 fsPosMid2 = new Vector3(0.5f, 0.5f,  0);
-    public Vector3 fsPosEnd  = new Vector3(1.0f, 0.65f, 0);
+    public float reloadDelay = 1f; // The delay between rounds
+
+    public Vector3 fsPosMid = new Vector3(0.5f, 0.90f, 0);
+    public Vector3 fsPosRun = new Vector3(0.5f, 0.75f, 0);
+    public Vector3 fsPosMid2 = new Vector3(0.5f, 0.5f, 0);
+    public Vector3 fsPosEnd = new Vector3(1.0f, 0.65f, 0);
+
+    public Text GTGameOver;
+    public Text GTRoundResult;
 
     public Deck deck;
     public TextAsset deckXML;
@@ -45,7 +50,9 @@ public class Prospector : MonoBehaviour
     public int chain = 0; // of cards in this run
     public int scoreRun = 0;
     public int score = 0;
-    public FloatingScore faRun;
+    public FloatingScore fsRun;
+
+
 
     void Awake()
     {
@@ -59,6 +66,31 @@ public class Prospector : MonoBehaviour
         score += SCORE_FROM_PREV_ROUND;
         // And reset the SCORE_FROM_PREV_ROUND
         SCORE_FROM_PREV_ROUND = 0;
+
+        // Set up the UI Texts that show at the end of the round
+        // Get the UI Text components
+        GameObject go = GameObject.Find("GameOver");
+        if (go != null)
+        {
+            GTGameOver = go.GetComponent<Text>();
+        }
+        go = GameObject.Find("RoundResult");
+        if (go != null)
+        {
+            GTRoundResult = go.GetComponent<Text>();
+        }
+        // Make them invisible
+        ShowResultGTs(false);
+
+        go = GameObject.Find("HighScore");
+        string hScore = "High Score: " + Utils.AddCommasToNumber(HIGH_SCORE);
+        go.GetComponent<Text>().text = hScore;
+    }
+
+    void ShowResultGTs(bool show)
+    {
+        GTGameOver.gameObject.SetActive(show);
+        GTRoundResult.gameObject.SetActive(show);
     }
 
     void Start()
@@ -110,16 +142,14 @@ public class Prospector : MonoBehaviour
             layoutAnchor.transform.position = layoutCenter;
         }
 
-
         CardProspector cp;
         // Follow the layout
         foreach (SlotDef tSD in layout.slotDefs)
         {
             // ^ Iterate through all the SlotDefs in the layout.slotDefs as tSD
             cp = Draw(); // Pull a card from the top (beginning) of the drawPile
-
-          //  cp.faceUp = tSD.faceUp; // Set its faceUp to the value in SlotDef
-           // cp.transform.parent = layoutAnchor; // Make its parent layoutAnchor
+            cp.faceUp = tSD.faceUp; // Set its faceUp to the value in SlotDef
+            cp.transform.parent = layoutAnchor; // Make its parent layoutAnchor
             // This replaces the previous parent: deck.deckAnchor, which appears as _Deck in the Hierarchy while the scene is playing
             cp.transform.localPosition = new Vector3(
                 layout.multiplier.x * tSD.x,
@@ -337,12 +367,21 @@ public class Prospector : MonoBehaviour
             // print("Game Over. You Lost. :(");
             ScoreManager(ScoreEvent.gameLoss); // Replaces the old line
         }
+        // Reload the scene in reloadDelay seconds
+        // This will give the score a moment to travel
+        Invoke("ReloadLevel", reloadDelay);
+        // SceneManager.LoadScene("__Prospector_Scene_0");
+    }
+
+    void ReloadLevel()
+    {
         // Reload the scene, resetting the game
         SceneManager.LoadScene("__Prospector_Scene_0");
     }
 
     // ScoreManager handles all of the scoring
-    void ScoreManager(ScoreEvent sEvt) {
+    void ScoreManager(ScoreEvent sEvt)
+    {
         List<Vector3> fsPts;
         switch (sEvt)
         {
@@ -353,40 +392,41 @@ public class Prospector : MonoBehaviour
                 chain = 0;          // Resets the score chain
                 score += scoreRun;  // Add scoreRun to total points
                 scoreRun = 0;       // Reset scoreRun
-                //AddnfsRun to the _scoreboard score
+                // Add fsRun to the _Scoreboard score
                 if (fsRun != null)
                 {
-                    //create points for the Bezier curve
+                    // Create points for the Bezier curve
                     fsPts = new List<Vector3>();
                     fsPts.Add(fsPosRun);
                     fsPts.Add(fsPosMid2);
                     fsPts.Add(fsPosEnd);
                     fsRun.reportFinishTo = Scoreboard.S.gameObject;
-                    fsPosRun.Init(fsPts, 0, 1);
-                    //also adjust the fontsize
-                    fsPosRun.fontSizes = new List<float> (new float[] { 28, 26, 4 });
+                    fsRun.Init(fsPts, 0, 1);
+                    // Also adjust the fontSize
+                    fsRun.fontSizes = new List<float>(new float[] { 24, 36, 4 });
+                    fsRun = null; // Clear fsRun so it's created again
                 }
                 break;
             case ScoreEvent.mine:       // Remove a card
                 chain++;            // Increase the score chain
-                scoreRun += chain; // Add score for this card to run
-                //create a FloatingScore for this score 
+                scoreRun += chain;  // Add score for this card to run
+                // Create a FloatingScore for this core
                 FloatingScore fs;
-                //Move it from the mousePosition to fsPosRun
+                // Move it from the mousePosition to fsPosRun
                 Vector3 p0 = Input.mousePosition;
                 p0.x /= Screen.width;
                 p0.y /= Screen.height;
                 fsPts = new List<Vector3>();
-                fsPts.Add(p0);
                 fsPts.Add(fsPosMid);
                 fsPts.Add(fsPosRun);
                 fs = Scoreboard.S.CreateFloatingScore(chain, fsPts);
-                fs.fontSizes = new List<float>(new float[] { 4, 50, 28 });
-                if (fsPosRun == null)
+                fs.fontSizes = new List<float>(new float[] { 4, 50, 24 });
+                if (fsRun == null)
                 {
                     fsRun = fs;
                     fsRun.reportFinishTo = null;
-                }else
+                }
+                else
                 {
                     fs.reportFinishTo = fsRun.gameObject;
                 }
@@ -397,23 +437,29 @@ public class Prospector : MonoBehaviour
         switch (sEvt)
         {
             case ScoreEvent.gameWin:
+                GTGameOver.text = "Round Over";
                 // If it's a win, add the score to the next round
                 // static fields are NOT reset by SceneManager.LoadScene()
                 Prospector.SCORE_FROM_PREV_ROUND = score;
                 print("You won this round! Round score: " + score);
                 break;
             case ScoreEvent.gameLoss:
+                GTGameOver.text = "Game Over";
                 // If it's a loss, check against the high score
                 if (Prospector.HIGH_SCORE <= score)
                 {
                     print("You got the high score! High score: " + score);
+                    string sRR = "You got the high score!\nHigh Score: " + score;
+                    GTRoundResult.text = sRR;
                     Prospector.HIGH_SCORE = score;
                     PlayerPrefs.SetInt("ProspectorHighScore", score);
                 }
                 else
                 {
                     print("Your final score for the game was: " + score);
+                    GTRoundResult.text = "Your final score was: " + score;
                 }
+                ShowResultGTs(true);
                 break;
             default:
                 print("score: " + score + " - scoreRun: " + scoreRun + " - chain: " + chain);
